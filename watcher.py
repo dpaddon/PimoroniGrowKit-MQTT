fc93e0c3d165de4fefc7a1fa2363556732818d03
Daniel <daniel.paddon@gmail.com>
2022-04-11 21:26:59 +0100

    added startup_wait period to config

--- watcher.py
+++ watcher.py
@@ -31,7 +31,7 @@ def auth():
     return load_config().get('auth')
 
 # Give time for the network to be initialised before starting up
-time.sleep(120)
+time.sleep(config.get("startup_wait", 0)
 
 config = load_config()
 broker = broker()
